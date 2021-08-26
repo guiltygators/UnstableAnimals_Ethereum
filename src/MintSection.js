8bc522a89fb09de1b66d9aeaae95b2142a865fc1
albenes <mendietap@icloud.com>
2021-08-26 08:08:02 -0600

    .

--- src/MintSection.js
+++ src/MintSection.js
@@ -47,10 +47,10 @@ function MintSection() {
   const [buyAmount, setBuyAmountValue] = useState(1)
   const [lastPurchasedUnstableAnimalsIds, setLastPurchasedUnstableAnimalsIds] = useState([])
 // temporalmente comento esta linea (volverla a activar luego) la que dice ready to mint
-  //const [appState, setAppState] = useState(APP_STATE.readyToMint)
+  const [appState, setAppState] = useState(APP_STATE.readyToMint)
 
 // borrar esta linea de .soldout solo esta para que en la pagina no este activa la compra
-  const [appState, setAppState] = useState(APP_STATE.soldOut)
+  //const [appState, setAppState] = useState(APP_STATE.soldOut)
 //  ^^^^^^^^^^^^^^^^^^^^^^^^^^ ESTA ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ BORRAR
 
   const [modalIsOpen, setModalOpen] = useState(false)
