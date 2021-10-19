6f5064f79309511ab031dab40333f5139f79fa66
albenes <mendietap@icloud.com>
2021-10-18 21:36:26 -0600

    design changes

--- src/TimelineSection.js
+++ src/TimelineSection.js
@@ -11,37 +11,39 @@ function TimelineSection() {
         <div class="container right">
             <div class="content">
                 <h2>0% Minted</h2>
-                <p></p>
+                <p>The Guilty Gators Penthouse is now open! Inside the Penthouse we will have member only game nights, merch drops, and more!</p>
             </div>
         </div>
         <div class="container right">
             <div class="content">
                 <h2>5% Minted</h2>
-                <p></p>
+                <p>We will re-invest all of the proceeds back into building a stronger Guilty Gator community and also host ETH giveaways to Guilty Gator holders.</p>
             </div>
         </div>
         <div class="container right">
             <div class="content">
                 <h2>10% Minted</h2>
-                <p></p>
+                <p>Guilty Gator Rarity.tools will be added to search and explore all the different kind of variations of your Gator! 300 very limited Baby Guilty Gators will be released and given for free (minus gas).</p>
             </div>
         </div>
         <div class="container right">
             <div class="content">
                 <h2>15% Minted</h2>
-                <p></p>
+                <p>The member exclusive Guilty Gator merch store will be opened. 200 more Baby Guilty Gators will be released to our holders. We will be donating $15,000 to a charity agreed upon by the Gator community.</p>
             </div>
         </div>
         <div class="container right">
             <div class="content">
                 <h2>25% Minted</h2>
-                <p></p>
+                <p>Hire (smarter people than us) to create more perks for Guilty Gator members. This includes development of the Guilty Gators metaverse.</p>
+                <p>Contests and ETH giveaways for holders.</p>
             </div>
         </div>
         <div class="container right">
             <div class="content">
                 <h2>50-100% Minted</h2>
-                <p></p>
+                <p>A Guilty Gator meta-verse will be open, more collaborations with different digital and non-digital artists, a shit-ton of giveaways back to the Gator community and the Guilty Gator takeover will begin. The rest of the Baby Guilty Gators will be released</p>
+                <p>Access to all future projects, exclusive NFT drops, and be able to help with community development.</p>
             </div>
         </div>
       </div>
