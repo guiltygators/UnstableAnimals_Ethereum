1598a6545c6c6bc9e12590fd37162ad50a210936
albenes <mendietap@icloud.com>
2021-10-18 21:56:14 -0600

    test roadmap texts

--- src/TimelineSection.js
+++ src/TimelineSection.js
@@ -11,40 +11,38 @@ function TimelineSection() {
         <div class="container right">
             <div class="content">
                 <h2>0% Minted</h2>
-                <p>The Guilty Gators Penthouse is now open! Inside the Penthouse we will have member only game nights, merch drops, and more!</p>
-            </div>
+                <p>The Guilty Gators Penthouse is now open! Feel free to join and mint you Guilty Gator to be apart of this exclusive Penthouse. Inside the Penthouse we will have member only game nights, merch drops, and more! The Penthouse will feature many tangible asset giveaways such as exclusive Baby Gator NFTs once mint goals are met! Come party with all the Guilty Gator holders and be apart of a private club. A ranking system will be implemented for Gators who participate in our chats with perks based on your standing. </p>
+                </div>
         </div>
         <div class="container right">
             <div class="content">
                 <h2>5% Minted</h2>
-                <p>We will re-invest all of the proceeds back into building a stronger Guilty Gator community and also host ETH giveaways to Guilty Gator holders.</p>
-            </div>
+                <p>We will re-invest all of the proceeds back into building a stronger Guilty Gator community and also host ETH giveaways to Guilty Gator holders. The Guilty Gator Penthouse will be filling up and ideas from all holders will be collected and taken into consideration for more Penthouse perks!</p>
+                </div>
         </div>
         <div class="container right">
             <div class="content">
                 <h2>10% Minted</h2>
-                <p>Guilty Gator Rarity.tools will be added to search and explore all the different kind of variations of your Gator! 300 very limited Baby Guilty Gators will be released and given for free (minus gas).</p>
-            </div>
+                <p>Guilty Gator Rarity.tools will be added to search and explore all the different kind of variations of your Gator! 300 very limited Baby Guilty Gators will be released and given for free (holders pay the gas) for penthouse members exclusively chosen based on Penthouse community rank! All minting proceeds until this point will still be reinvested into the project and used for extra incentives for Guilty Gator holders and other resources to grow our project together.</p>
+                </div>
         </div>
         <div class="container right">
             <div class="content">
                 <h2>15% Minted</h2>
-                <p>The member exclusive Guilty Gator merch store will be opened. 200 more Baby Guilty Gators will be released to our holders. We will be donating $15,000 to a charity agreed upon by the Gator community.</p>
-            </div>
+                <p>The member exclusive Guilty Gator merch store will be opened. 200 more Baby Guilty Gators will be released to our holders. We will be donating $15,000 to a charity agreed upon by the Gator community. If there are two charities that are well liked and voted on, f*ck it; another $15,000. We will be using most of the funds minted to partner up with other rising NFT projects and influencers.</p>
+                </div>
         </div>
         <div class="container right">
             <div class="content">
                 <h2>25% Minted</h2>
-                <p>Hire (smarter people than us) to create more perks for Guilty Gator members. This includes development of the Guilty Gators metaverse.</p>
-                <p>Contests and ETH giveaways for holders.</p>
+                <p>Hire (smarter people than us) to create more perks for Guilty Gator members. This includes development of the Guilty Gators metaverse. The gators will do a victory lap around the blockchain. We open up a contest open to all for an exclusive Gator NFT drawn by one of you! If you’re into art and design, join the contest and your design might be chosen for the 1/1 Gator. The winner of that contest will receive ALL the proceeds from the Exclusive Gator Auction. Of course, more ETH Giveaways to the Guilty Gator members.</p>
             </div>
         </div>
         <div class="container right">
             <div class="content">
                 <h2>50-100% Minted</h2>
-                <p>A Guilty Gator meta-verse will be open, more collaborations with different digital and non-digital artists, a shit-ton of giveaways back to the Gator community and the Guilty Gator takeover will begin. The rest of the Baby Guilty Gators will be released</p>
-                <p>Access to all future projects, exclusive NFT drops, and be able to help with community development.</p>
-            </div>
+                <o>A Guilty Gator meta-verse will be open, more collaborations with different digital and non-digital artists, a shit-ton of giveaways back to the Gator community and the Guilty Gator takeover will begin. The rest of the Baby Guilty Gators will be released and given for free to all Gator holders (holders pay the gas). As technology expands in the NFT and blockchain space, The Guilty Gator team will find new ways and adapt to the current status quo of the NFT space. The Gators are here to stay for the long run. The Guilty Gators will exist for as long as they have existed on earth. All Gator holders will have first access to all future projects, exclusive NFT drops, and be able to help with community development.</o>
+                </div>
         </div>
       </div>
 </div>
